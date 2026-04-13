d3cca43d61ed6b6cae6a2c8f0adbb289468d3fdd
Rizal Hilman <rizal.hilman67@gmail.com>
2026-04-13 09:58:35 +0700

    Initial Commit

---
+++ Starter Music Playlist/Starter_Music_PlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starter_Music_PlaylistApp.swift
+//  Starter Music Playlist
+//
+//  Created by Rizal Hilman on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Starter_Music_PlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
